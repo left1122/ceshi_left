3f83c6d06cab4eb55e1a9fd06155c08e834315e9
left1122 <left1122@126.com>
2017-03-20 13:44:03 +0800

    left update02

--- src/test_left/test_01.java
+++ src/test_left/test_01.java
@@ -4,5 +4,8 @@ public class test_01 {
 
 	public static void main(String[] args) {
 		System.out.println("1111111111111111");
+		System.out.println("3333333333333333");
+		System.out.println("3333333333333333");
+		System.out.println("3333333333333333");
 	}
 }
